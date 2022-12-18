7b87e688f71ad9349b3f319f7c71d245483515fd
NewUser <hammadmalli64@gmail.com>
2022-12-18 20:26:16 +0500

    upcoming screen

--- lib/screens/upcoming/upcoming_screen.dart
+++ lib/screens/upcoming/upcoming_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../utils/colors.dart';
 
 class UpcommingScreen extends StatelessWidget {
   const UpcommingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ();
+    return Container(
+      height: 750.h,
+      width: 300.w,
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(
+              BorderSide(
+                  color: AppColors.kSecondryColor
+              )
+          )
+      ),
+    );
   }
 }
